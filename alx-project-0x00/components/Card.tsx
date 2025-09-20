2e956b2bcf6a9202d0718c85df4bd17c9f8bac22
Saadia Nazih <saadia.nazih92@gmail.com>
2025-09-20 13:32:19 +0200

    new updae

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -1,8 +1,8 @@
 // File: components/Card.tsx
 
 import Image from "next/image"
-import HOUSE_IMAGE from "@/public/assets/house.png"
-import STAR_IMAGE from "@/public/assets/star.png"
+import HOUSE_IMAGE from "@/public/assets/images/house.png"
+import STAR_IMAGE from "@/public/assets/images/star.png"
 import React from "react"
 import Pill from "./Pill"
 
